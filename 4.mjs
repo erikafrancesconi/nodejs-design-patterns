0f7afb56270a0c8f28ec3504716d7f59a53e26b7
Erika Francesconi <fiorediren@gmail.com>
2021-07-05 16:05:56 +0200

    4.2

--- 4.mjs
+++ 4.mjs
@@ -1,4 +1,5 @@
-import { appendFile, readFile, readdir, lstat } from "fs";
+import { appendFile, readFile, readdir, lstatSync } from "fs";
+import { dirname } from "path";
 
 // 4.1: File concatenation
 // write the implementation of concatFiles(), a
@@ -36,8 +37,63 @@ concatFiles(
     if (err) {
       return console.error(err);
     }
-    console.log("Finished");
+    // console.log("Finished");
   },
   "files/fileA.txt",
   "files/fileB.json"
 );
+
+// 4.2 List files recursively
+// Write listNestedFiles(), a callback-style function
+// that takes, as the input, the path to a directory in the local filesystem and that
+// asynchronously iterates over all the subdirectories to eventually return a list
+// of all the files discovered.
+// Bonus points if you manage to avoid callback hell. Feel free to create
+// additional helper functions if needed.
+const listFiles = (dir, cb) => {
+  const fileList = [];
+
+  readdir(dir, (err, files) => {
+    if (err) {
+      return cb(err);
+    }
+
+    files.forEach((file) => {
+      if (lstatSync(`${dir}/${file}`).isFile()) {
+        fileList.push(`${dir}/${file}`);
+      } else {
+        listNestedFiles(`${dir}/${file}`, cb, `${dir}/${file}`);
+      }
+    });
+
+    return cb(null, fileList);
+  });
+};
+
+const listNestedFiles = (dir, cb) => {
+  const done = (err, files) => {
+    if (err) {
+      return cb(err);
+    }
+    return cb(err, files, dir);
+  };
+
+  listFiles(dir, done);
+};
+
+const listAll = () => {
+  let allFiles = [];
+
+  listNestedFiles("/home/erika/testdir", (err, files) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    if (files.length > 0) {
+      allFiles = allFiles.concat(files);
+      console.log(allFiles);
+    }
+  });
+};
+
+listAll();
